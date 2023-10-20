f9f1ca760b18eb4dcf7efbb92397c6c25f9bff06
Omkar Kamale <check.omkar@gmail.com>
2023-10-21 00:28:24 +0530

    Updates and tests

---
+++ test/fuzz/InvariantsTest.t.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Test} from "forge-std/Test.sol";
+import {StdInvariant} from "forge-std/StdInvariant.sol";
+import {DSCEngine} from "../../../src/DSCEngine.sol";
+import {DecentralizedStableCoin} from "../../../src/DecentralizedStableCoin.sol";
+import {HelperConfig} from "../../../script/HelperConfig.s.sol";
+import {DeployDSC} from "../../../script/DeployDSC.s.sol";
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/ERC20Mock.sol";
+// import {ContinueOnRevertHandler} from "./ContinueOnRevertHandler.t.sol";
+import {console} from "forge-std/console.sol";
+
+contract InvariantsTest is StdInvariant, Test {
+    function setUp() external {
+        DeployDSC deployer = new DeployDSC();
+        (dsc, dsce, helperConfig) = deployer.run();
+        (ethUsdPriceFeed, btcUsdPriceFeed, weth, wbtc) = helperConfig
+            .activeNetworkConfig();
+        handler = new ContinueOnRevertHandler(dsce, dsc);
+        targetContract(address(handler));
+        // targetContract(address(ethUsdPriceFeed)); Why can't we just do this?
+    }
+
+    function invariant_protocolMustHaveMoreValueThatTotalSupplyDollars()
+        public
+        view
+    {
+        uint256 totalSupply = dsc.totalSupply();
+        uint256 wethDeposted = ERC20Mock(weth).balanceOf(address(dsce));
+        uint256 wbtcDeposited = ERC20Mock(wbtc).balanceOf(address(dsce));
+
+        uint256 wethValue = dsce.getUsdValue(weth, wethDeposted);
+        uint256 wbtcValue = dsce.getUsdValue(wbtc, wbtcDeposited);
+
+        console.log("wethValue: %s", wethValue);
+        console.log("wbtcValue: %s", wbtcValue);
+
+        assert(wethValue + wbtcValue >= totalSupply);
+    }
+}
